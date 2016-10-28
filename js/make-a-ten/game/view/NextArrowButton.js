5dc0472f757380d05d9b981301f87341baf199db
Jonathan Olson <olsonsjc@gmail.com>
2016-10-27 22:29:47 -0600

    NextArrowButton cleanup

--- js/make-a-ten/game/view/NextArrowButton.js
+++ js/make-a-ten/game/view/NextArrowButton.js
@@ -1,7 +1,7 @@
 // Copyright 2015, University of Colorado Boulder
 
 /**
- * Button for showing "next" label and a arrow button
+ * Button for showing "next" label and a arrow button.
  *
  * @author Sharfudeen Ashraf
  * @author John Blanco
@@ -25,21 +25,12 @@ define( function( require ) {
   var LABEL_FONT = new PhetFont( { size: 20, weight: 'bold' } );
 
   /**
+   * @constructor
+   *
    * @param {string} buttonText
    * @param {Object} [options]
-   * @constructor
    */
   function NextArrowButton( buttonText, options ) {
-
-    options = _.extend( {
-
-      xMargin: 12,
-      yMargin: 10,
-
-      // Default base code matches the yellow in the PhET logo (the one with the paper airplane).
-      baseColor: new Color( 242, 233, 22 )
-
-    }, options );
 
     var arrowShape = new ArrowShape( 0, 0, 28.5, 0, {
       tailWidth: 2,
@@ -47,15 +38,19 @@ define( function( require ) {
       headHeight: 8
     } );
 
-    var labelNode = new Text( buttonText, { font: LABEL_FONT, fill: 'black' } );
-    var arrowPath = new Path( arrowShape, { fill: 'black' } );
-
     var labelArrowBox = new HBox( {
-      children: [ labelNode, arrowPath ],
+      children: [
+        new Text( buttonText, { font: LABEL_FONT, fill: 'black' } ),
+        new Path( arrowShape, { fill: 'black' } )
+      ],
       spacing: 15
     } );
 
     RectangularPushButton.call( this, _.extend( {
+      // Default base code matches the yellow in the PhET logo (the one with the paper airplane).
+      baseColor: new Color( 242, 233, 22 ),
+      xMargin: 12,
+      yMargin: 10,
       content: labelArrowBox
     }, options ) );
   }
