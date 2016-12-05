5c16fa7c3ed46a8c56e0d60fe8975773c8f0f14d
Jonathan Olson <olsonsjc@gmail.com>
2016-12-05 13:26:42 -0700

    Moving the line between 'pull apart' and 'move', see https://github.com/phetsims/make-a-ten/issues/252

--- js/make-a-ten/common/MakeATenConstants.js
+++ js/make-a-ten/common/MakeATenConstants.js
@@ -25,7 +25,7 @@ define( function( require ) {
      * {number} - Where is the boundary between paper number "move" targets and "split" targets, where 0 would be the
      * bottom of the paper number and 1 would be the top.
      */
-    SPLIT_BOUNDARY_HEIGHT_PROPORTION: 0.38,
+    SPLIT_BOUNDARY_HEIGHT_PROPORTION: 0.55,
 
     /**
      * {number} - View coordinates per second for animation
