72ccbc5def61c3f20039aa1937177014d62c02ff
samreid <reids@colorado.edu>
2017-11-09 17:09:27 -0700

    Updated copyright statement, see https://github.com/phetsims/tasks/issues/902

--- js/make-a-ten/adding/model/ActiveTerm.js
+++ js/make-a-ten/adding/model/ActiveTerm.js
@@ -1,4 +1,4 @@
-// Copyright 2016, University of Colorado Boulder
+// Copyright 2016-2017, University of Colorado Boulder
 
 /**
  * Enumeration for which term is actively being edited in the Adding screen.
