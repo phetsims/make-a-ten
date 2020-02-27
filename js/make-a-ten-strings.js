f602fcf65f27ee7595fb0334a2cf2ab7f0d3ec31
samreid <reids@colorado.edu>
2020-02-27 12:08:01 -0700

    Add images and strings, see https://github.com/phetsims/chipper/issues/875

---
+++ js/make-a-ten-strings.js
@@ -0,0 +1,14 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+
+import getStringModule from '../../chipper/js/getStringModule.js';
+import makeATen from './makeATen.js';
+
+const makeATenStrings = getStringModule( 'MAKE_A_TEN' );
+
+makeATen.register( 'makeATenStrings', makeATenStrings );
+
+export default makeATenStrings;
