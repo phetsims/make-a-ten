0423bb11ea71be7faee259d12ed26df517359fd4
Jonathan Olson <jonathan.olson@colorado.edu>
2020-05-27 16:40:16 -0600

    Using ScreenIcon, see https://github.com/phetsims/joist/issues/626

--- js/make-a-ten/common/MakeATenUtils.js
+++ js/make-a-ten/common/MakeATenUtils.js
@@ -6,6 +6,7 @@
  * @author Sharfudeen Ashraf
  */
 
+import ScreenIcon from '../../../../joist/js/ScreenIcon.js';
 import Image from '../../../../scenery/js/nodes/Image.js';
 import Node from '../../../../scenery/js/nodes/Node.js';
 import Rectangle from '../../../../scenery/js/nodes/Rectangle.js';
@@ -36,13 +37,16 @@ const MakeATenUtils = {
   createIconWithBackgroundColor: function( image, backgroundFill ) {
     const imageNode = new Image( image );
 
-    return new Node( {
+    return new ScreenIcon( new Node( {
       children: [
         new Rectangle( 0, 0, imageNode.imageWidth, imageNode.imageHeight, {
           fill: backgroundFill
         } ),
         imageNode
       ]
+    } ), {
+      maxIconWidthProportion: 1,
+      maxIconHeightProportion: 1
     } );
   }
 };
