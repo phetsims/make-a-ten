5b1d7c0ef9abbc3a22402a35c05df978215f4df8
pixelzoom <cmalley@pixelzoom.com>
2020-09-17 00:16:55 -0600

    convert RewardDialog to ES6, https://github.com/phetsims/vegas/issues/82

--- js/make-a-ten/game/view/MakeATenRewardNode.js
+++ js/make-a-ten/game/view/MakeATenRewardNode.js
@@ -6,7 +6,6 @@
  * @author Jonathan Olson <jonathan.olson@colorado.edu>
  */
 
-import inherit from '../../../../../phet-core/js/inherit.js';
 import FaceNode from '../../../../../scenery-phet/js/FaceNode.js';
 import StarNode from '../../../../../scenery-phet/js/StarNode.js';
 import RewardNode from '../../../../../vegas/js/RewardNode.js';
@@ -14,51 +13,48 @@ import makeATen from '../../../makeATen.js';
 import BaseNumber from '../../common/model/BaseNumber.js';
 import BaseNumberNode from '../../common/view/BaseNumberNode.js';
 
+class MakeATenRewardNode extends RewardNode {
+  constructor() {
+    super( {
+      nodes: RewardNode.createRandomNodes( [
+        new StarNode(),
+        new StarNode(),
+        new StarNode(),
+        new StarNode(),
+        new StarNode(),
+        new StarNode(),
+        new StarNode(),
+        new FaceNode( 40, { headStroke: 'black' } ),
+        new FaceNode( 40, { headStroke: 'black' } ),
+        new FaceNode( 40, { headStroke: 'black' } ),
+        new FaceNode( 40, { headStroke: 'black' } ),
+        new FaceNode( 40, { headStroke: 'black' } ),
+        new FaceNode( 40, { headStroke: 'black' } ),
+        new FaceNode( 40, { headStroke: 'black' } ),
+        createNumber( 1, 0 ),
+        createNumber( 2, 0 ),
+        createNumber( 3, 0 ),
+        createNumber( 4, 0 ),
+        createNumber( 5, 0 ),
+        createNumber( 6, 0 ),
+        createNumber( 7, 0 ),
+        createNumber( 8, 0 ),
+        createNumber( 9, 0 ),
+        createNumber( 1, 1 ),
+        createNumber( 1, 1 ),
+        createNumber( 1, 1 ),
+        createNumber( 1, 1 ),
+        createNumber( 1, 1 )
+      ], 150 )
+    } );
+  }
+}
+
 function createNumber( digit, place ) {
   const node = new BaseNumberNode( new BaseNumber( digit, place ), 1 );
   node.scale( 0.5 );
   return node;
 }
 
-/**
- * @constructor
- */
-function MakeATenRewardNode() {
-  RewardNode.call( this, {
-    nodes: RewardNode.createRandomNodes( [
-      new StarNode(),
-      new StarNode(),
-      new StarNode(),
-      new StarNode(),
-      new StarNode(),
-      new StarNode(),
-      new StarNode(),
-      new FaceNode( 40, { headStroke: 'black' } ),
-      new FaceNode( 40, { headStroke: 'black' } ),
-      new FaceNode( 40, { headStroke: 'black' } ),
-      new FaceNode( 40, { headStroke: 'black' } ),
-      new FaceNode( 40, { headStroke: 'black' } ),
-      new FaceNode( 40, { headStroke: 'black' } ),
-      new FaceNode( 40, { headStroke: 'black' } ),
-      createNumber( 1, 0 ),
-      createNumber( 2, 0 ),
-      createNumber( 3, 0 ),
-      createNumber( 4, 0 ),
-      createNumber( 5, 0 ),
-      createNumber( 6, 0 ),
-      createNumber( 7, 0 ),
-      createNumber( 8, 0 ),
-      createNumber( 9, 0 ),
-      createNumber( 1, 1 ),
-      createNumber( 1, 1 ),
-      createNumber( 1, 1 ),
-      createNumber( 1, 1 ),
-      createNumber( 1, 1 )
-    ], 150 )
-  } );
-}
-
 makeATen.register( 'MakeATenRewardNode', MakeATenRewardNode );
-
-inherit( RewardNode, MakeATenRewardNode );
 export default MakeATenRewardNode;
