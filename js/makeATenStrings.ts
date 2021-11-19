72b6ae1d4031d282a2180f73a786733556ac33d6
Sam Reid <reids@colorado.edu>
2021-11-18 17:44:06 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/makeATenStrings.ts
@@ -0,0 +1,43 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import makeATen from './makeATen.js';
+
+type StringsType = {
+  'make-a-ten': {
+    'title': string
+  },
+  'screen': {
+    'adding': string,
+    'explore': string,
+    'game': string
+  },
+  'hideTotal': string,
+  'submit': string,
+  'next': string,
+  'pattern': {
+    'level': {
+      '0levelNumber': string
+    }
+  },
+  'level1Description': string,
+  'level2Description': string,
+  'level3Description': string,
+  'level4Description': string,
+  'level5Description': string,
+  'level6Description': string,
+  'level7Description': string,
+  'level8Description': string,
+  'level9Description': string,
+  'level10Description': string
+};
+
+const makeATenStrings = getStringModule( 'MAKE_A_TEN' ) as StringsType;
+
+makeATen.register( 'makeATenStrings', makeATenStrings );
+
+export default makeATenStrings;
