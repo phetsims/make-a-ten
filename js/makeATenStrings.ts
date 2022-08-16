8474a38205db6c2b6987e11fc0080c864e76495c
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:41 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/makeATenStrings.ts
+++ js/makeATenStrings.ts
@@ -5,35 +5,54 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import makeATen from './makeATen.js';
 
 type StringsType = {
   'make-a-ten': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'screen': {
     'adding': string;
+    'addingProperty': TReadOnlyProperty<string>;
     'explore': string;
+    'exploreProperty': TReadOnlyProperty<string>;
     'game': string;
+    'gameProperty': TReadOnlyProperty<string>;
   };
   'hideTotal': string;
+  'hideTotalProperty': TReadOnlyProperty<string>;
   'submit': string;
+  'submitProperty': TReadOnlyProperty<string>;
   'next': string;
+  'nextProperty': TReadOnlyProperty<string>;
   'pattern': {
     'level': {
       '0levelNumber': string;
+      '0levelNumberProperty': TReadOnlyProperty<string>;
     }
   };
   'level1Description': string;
+  'level1DescriptionProperty': TReadOnlyProperty<string>;
   'level2Description': string;
+  'level2DescriptionProperty': TReadOnlyProperty<string>;
   'level3Description': string;
+  'level3DescriptionProperty': TReadOnlyProperty<string>;
   'level4Description': string;
+  'level4DescriptionProperty': TReadOnlyProperty<string>;
   'level5Description': string;
+  'level5DescriptionProperty': TReadOnlyProperty<string>;
   'level6Description': string;
+  'level6DescriptionProperty': TReadOnlyProperty<string>;
   'level7Description': string;
+  'level7DescriptionProperty': TReadOnlyProperty<string>;
   'level8Description': string;
+  'level8DescriptionProperty': TReadOnlyProperty<string>;
   'level9Description': string;
+  'level9DescriptionProperty': TReadOnlyProperty<string>;
   'level10Description': string;
+  'level10DescriptionProperty': TReadOnlyProperty<string>;
 };
 
 const makeATenStrings = getStringModule( 'MAKE_A_TEN' ) as StringsType;
