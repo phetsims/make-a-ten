5c8fd625fcbc7fa554977df6110885981084734c
phet-dev <phetadmin@gmail.com>
2022-10-28 04:19:56 -0600

    update copyright dates from daily grunt work

--- js/make-a-ten/explore/model/Cue.js
+++ js/make-a-ten/explore/model/Cue.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2021, University of Colorado Boulder
+// Copyright 2016-2022, University of Colorado Boulder
 
 /**
  * Common move/split cue model. The cue represents a visual indicator that sticks to a counting object, and lets the user
