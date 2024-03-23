fb7f38d601bf0cb1dc83080a022a8b204c0f6a1f
phet-dev <phetadmin@gmail.com>
2024-03-23 03:22:42 -0600

    update copyright dates from daily grunt work

--- js/make-a-ten/explore/view/ExplorePanel.js
+++ js/make-a-ten/explore/view/ExplorePanel.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2023, University of Colorado Boulder
+// Copyright 2016-2024, University of Colorado Boulder
 
 /**
  * Panel that contains a 100, 10 and 1, which can be clicked/dragged to create draggable counting objects.
